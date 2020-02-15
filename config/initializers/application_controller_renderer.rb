237899359444efd58df85c35f3fabc36cf0f37ad
吉田　充秀 <mauser_c96@train.ocn.ne.jp>
2020-02-15 19:18:09 +0900

    [Finish]5-8

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '1c9a01cf0c3a4100a83ff8cc8631901e48624a1ac9eda9d7d40fbd10a56e3fe90d1900d9cf9254c8369b9fe23ead2298540ed24896bedeab1a1991d4f843c30d'
